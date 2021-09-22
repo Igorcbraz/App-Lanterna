e39693d970d17f5fc9cf392045668f9cb8578a82
Igor Costa Braz <Igorcbraz1@gmail.com>
2021-09-22 15:30:59 -0300

    First Commit

---
+++ Lanterna/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lanterna {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void btnOnOff_Clicked(object sender, EventArgs e) {
+
+        }
+    }
+}
